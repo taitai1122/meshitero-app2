26129cd0647b20d258c4c370de5858a41f73c1ed
taitai1122 <taikicody592@icloud.com>
2021-09-19 13:03:05 +0000

    Initial commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ac35b3d5469878ea6b454c0f9f9b9ac4a8f3ba8fa3635041e97cb4997f752b4ebdf20740a43df32e8ac9ef3f63d3633fcb2e1cb0f48f9be9f1e96d99e2869cd6'
